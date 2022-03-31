b3c28eeeb4d7709f07f91a8a6c4ef19bfc7326b1
Igor Ivashkevich <ivashkevich.java@gmail.com>
2022-03-31 15:42:06 +0300

    finish lesson 24

--- src/main/java/ru/ivashkevich/springcourse/controllers/PeopleController.java
+++ src/main/java/ru/ivashkevich/springcourse/controllers/PeopleController.java
@@ -1,8 +1,10 @@
 package ru.ivashkevich.springcourse.controllers;
 
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import ru.ivashkevich.springcourse.dao.PersonDAO;
 import ru.ivashkevich.springcourse.models.Person;
@@ -43,13 +45,18 @@ public class PeopleController {
     }
 
     @PostMapping()
-    public String create(@ModelAttribute("person") Person person){
+    public String create(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult){
+        if (bindingResult.hasErrors())
+            return "people/new";
+
         personDAO.save(person);
         return "redirect:/people";
     }
 
     @PatchMapping("/{id}")
-    public String update(@ModelAttribute("person") Person person, @PathVariable("id") int id){
+    public String update(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult, @PathVariable("id") int id){
+        if(bindingResult.hasErrors())
+            return "people/edit";
         personDAO.update(id, person);
         return "redirect:/people";
     }

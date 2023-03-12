931202bc7c50630d33d34ff509744e070a543034
IvashkevichIgor <ivashkevich.java@gmail.com>
2023-03-12 17:32:37 +0300

    after lesson 58 add hibernate in project instead of JDBCTemplate

--- src/main/java/ru/ivashkevich/springcourse/models/Person.java
+++ src/main/java/ru/ivashkevich/springcourse/models/Person.java
@@ -1,33 +1,40 @@
 package ru.ivashkevich.springcourse.models;
 
+import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
+@Entity
+@Table(name = "person")
 public class Person {
 
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @NotEmpty(message = "Name should not be empty")
     @Size(min = 2, max = 30, message = "Name should be between 2 and 30 characters")
+    @Column(name = "name")
     private String name;
 
     @Min(value = 0, message = "Age should not be negative")
+    @Column(name = "age")
     private int age;
 
-    @NotEmpty(message = "Email should not be empty")
+    /*@NotEmpty(message = "Email should not be empty")
     @Email(message = "Email should be valid")
-    private String email;
+    @Column(name = "email")
+    private String email;*/
 
     public Person() {
     }
 
-    public Person(int id, String name, int age, String email) {
-        this.id = id;
+    public Person(String name, int age) {
         this.name = name;
         this.age = age;
-        this.email = email;
     }
 
     public int getId() {
@@ -52,13 +59,5 @@ public class Person {
 
     public void setAge(int age) {
         this.age = age;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 }
